7ce66cefb106e155893a8c955d77fce07fbccf65
Kathleen Kamali <kathleenkamali@gmail.com>
2020-02-11 19:52:43 -0800

    added the right routes (for now)

--- demon-summoning-api/config/routes.rb
+++ demon-summoning-api/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :affections
-  resources :dialogues
-  resources :sacrifices
-  resources :summons
-  resources :demons
-  resources :players
+  resources :affections, only: [:only, :edit]
+  resources :dialogues, only: [:show]
+  resources :summons, only: [:index, :create]
+  resources :players, only: [:show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
